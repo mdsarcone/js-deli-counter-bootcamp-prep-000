46cee253c6ff7add07205b4ef008b962e6d0b9e4
Mark Sarcone <mdsarcone@gmail.com>
2017-09-05 03:25:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ function takeANumber(katzDeliLine,name) {
   katzDeliLine.push(name)
   return "Welcome, " + name + ". You are number " + (katzDeliLine.indexOf(name) + 1) + " in line."
 }
+
+function nowServing() {
+  var servingNow = ""
+  if (katzDeliLine.length === 0) {
+    servingNow = "There is nobody waiting to be served!"
+  }
+  else {
+    servingNow = "Currentlly serving " + katzDeliLine[0] + "."
+    katzDeliLine.shift()
+  }
+  return servingNow
+}
